a941a8efa85069094a3166ab469bbc1546f058e0
Joshua O'Brien <josh@deveire.com>
2021-01-24 17:36:19 +0000

    Add Patron Button

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Game />
+        <div className="mt-4">
+        <a href="https://www.patreon.com/bePatron?u=23475911" data-patreon-widget-type="become-patron-button">Become a Patron!</a>
+        </div>
       </header>
     </div>
   );
