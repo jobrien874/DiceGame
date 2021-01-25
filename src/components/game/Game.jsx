90d5ee4d2402207194db5c8a32261aded142ddd7
Joshua O'Brien <josh@deveire.com>
2021-01-25 11:51:18 +0000

    Edits for other games

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -9,7 +9,7 @@ class Game extends Component {
     diceCount: 1,
     diceNumbers: [1],
     total: 1,
-    maxDiceAmount: [1, 2, 3, 4, 5, 6, 10, 50, 100],
+    maxDiceAmount: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 50, 100],
     diceStyle: ["Regular", "Chaos", "Wood"],
     sameNumber: false,
   };
