735e33d87bef07c726e63d264341258190742687
Joshua O'Brien <josh@deveire.com>
2021-02-23 17:09:46 +0000

    Header

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
-import Game from './components/game/Game'
-import './App.css';
+import Game from "./components/game/Game";
+import "./App.css";
 
 function App() {
-
-
   return (
     <div className="App">
+      <header>
+        <div className="navbar navbar-dark bg-dark shadow-sm">
+          <div className="container d-flex justify-content-between">
+            <a href="/" className="navbar-brand d-flex align-items-center">
+              <strong>DiceRoll</strong>
+            </a>
+          </div>
+        </div>
+      </header>
       <div className="App-body">
         <Game />
         <div className="mt-4">
-        <a href="https://www.patreon.com/bePatron?u=23475911" data-patreon-widget-type="become-patron-button">Become a Patron!</a>
+          <a
+            href="https://www.patreon.com/bePatron?u=23475911"
+            data-patreon-widget-type="become-patron-button"
+          >
+            Become a Patron!
+          </a>
         </div>
       </div>
       <footer className="App-footer container-fluid  mt-auto">
-            <p className="PatreonMessage">Help Keep Dice Roll Free From Ads and Contribute Towards my Patreon</p>
-            <p className="text-center">By <a href="https://joshob.ie">Josh O'Brien - joshob.ie</a></p>
-        </footer>
+        <p className="PatreonMessage">
+          Help Keep Dice Roll Free From Ads and Contribute Towards my Patreon
+        </p>
+        <p className="text-center">
+          By <a href="https://joshob.ie">Josh O'Brien - joshob.ie</a>
+        </p>
+      </footer>
     </div>
   );
 }
