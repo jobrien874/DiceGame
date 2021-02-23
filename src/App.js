56db085bcbb20af211400b7cb47623371e7fa0e1
Joshua O'Brien <josh@deveire.com>
2021-02-23 17:02:39 +0000

    Message

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
         </div>
       </div>
       <footer className="App-footer container-fluid  mt-auto">
+            <p className="PatreonMessage">Help Keep Dice Roll Free From Ads and Contribute Towards my Patreon</p>
             <p className="text-center">By <a href="https://joshob.ie">Josh O'Brien - joshob.ie</a></p>
         </footer>
     </div>
