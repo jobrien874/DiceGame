f84941a9fea7801f2f92dbbe5b027e9e8e70ae3a
Josh O'Brien <jobrien874@gmail.com>
2022-02-18 22:38:06 +0000

    About changes

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -3,7 +3,12 @@ import React, { Component } from "react";
 class About extends Component {
   state = {};
   render() {
-    return <div className="App-body"></div>;
+    return <div className="App-body">
+      <div className="About-container">
+      <h1>About</h1>
+      <p>A little project that hopefully someone finds handy if they need dice for a Board Game, Dungeons and Dragons or literally anything else.</p>
+      </div>
+    </div>;
   }
 }
 
